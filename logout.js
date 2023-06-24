47c1f161fc7da833213e81fc162061e93c8c901c
Ricardo Machado <ricardo.machado@ua.pt>
2023-06-24 18:44:52 +0100

    login

--- logout.js
+++
@@ -1,8 +0,0 @@
-var logoutButton = document.getElementById("logout");
-if (logoutButton) {
-    logoutButton.addEventListener("click", () => {
-        console.log("clicked");
-        localStorage.setItem("auth", false);
-        window.location.href = "index.html";
-    });
-}
